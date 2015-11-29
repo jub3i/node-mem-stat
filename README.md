f553380655a9211c457acdc1caea405c80f3d069
jub3i <anon66@gmail.com>
2015-11-30 00:49:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,114 @@
+mem-stat
+========
+
+```
+  ___ ___ _______ ___ ___ _______ _______ _______ _______
+ |   Y   |   _   |   Y   |   _   |       |   _   |       |
+ |.      |.  1___|.      |   1___|.|   | |.  1   |.|   | |
+ |. \_/  |.  __)_|. \_/  |____   `-|.  |-|.  _   `-|.  |-'
+ |:  |   |:  1   |:  |   |:  1   | |:  | |:  |   | |:  |
+ |::.|:. |::.. . |::.|:. |::.. . | |::.| |::.|:. | |::.|
+ `--- ---`-------`--- ---`-------' `---' `--- ---' `---'
+```
+
+**Note:** This repo can be found on npm here: [mem-stat](https://www.npmjs.com/package/mem-stat)
+
+**Caveat:** Works by parsing `/proc/meminfo` - so will only work on nix OS.
+
+Install
+-------
+
+```
+npm install mem-stat
+```
+
+Example
+-------
+
+```
+var memStat = require('mem-stat');
+
+//by default returns values in bytes
+var free = memStat.free();
+console.log(free);
+
+//return value in GiB, see docs below for allowed values of units
+var free = memStat.free('GiB');
+console.log(free);
+
+//returns percentage used eg. `13.359272`
+var usedPercent = memStat.usedPercent('GiB');
+console.log(usedPercent);
+
+//get all fields available from `/proc/meminfo`
+var allStats = memStat.raw();
+console.log(allStats);
+```
+
+free(units)
+-----------
+
+Returns the amount of free memory in `units` avaialable on the system.
+
+Option        | Type         | Default       | Explanation
+------------- | -------------| ------------- | ------------
+units         | `String`     | `'bytes'`     | The units of the returned value, can be one of `bytes`, `KiB`, `MiB` or `GiB`.
+
+total(units)
+------------
+
+Returns the amount of total memory in `units` avaialable on the system. It's basically the same as `os.totalmem()`, but instead using `/proc/meminfo`.
+
+Option        | Type         | Default       | Explanation
+------------- | -------------| ------------- | ------------
+units         | `String`     | `'bytes'`     | The units of the returned value, can be one of `bytes`, `KiB`, `MiB` or `GiB`.
+
+freePercent()
+-------------
+
+Returns the amount of free memory as a percentage eg `83.9430437`.
+
+freePercent()
+-------------
+
+Returns the amount of used memory as a percentage eg `13.9430437`.
+
+allStats(units)
+---------------
+
+Returns an object of all the stats, therfore only requiring one reading and parsing of `/proc/meminfo`.
+
+```
+  {
+    free: 1000,
+    total: 8000,
+    freePercent: 86.245977,
+    usedPercent: 13.754023,
+  }
+```
+
+Option        | Type         | Default       | Explanation
+------------- | -------------| ------------- | ------------
+units         | `String`     | `'bytes'`     | The units of the returned value, can be one of `bytes`, `KiB`, `MiB` or `GiB`.
+
+raw()
+-----
+
+Returns an object representing the data in `/proc/meminfo`.
+
+Contributing
+------------
+
+Just send a PR, or create an issue if you are not sure.
+
+Areas ripe for contribution:
+- testing
+- cross compatability for windows and darwin/osx
+- performance
+- bugs
+
+License
+-------
+
+MIT
+
---
+++ index.js
@@ -0,0 +1,173 @@
+var fs = require('fs');
+
+module.exports = {
+  free: free,
+  total: total,
+  freePercent: freePercent,
+  usedPercent: usedPercent,
+  allStats: allStats,
+  raw: raw,
+};
+
+/* PUBLIC */
+
+function free(units) {
+  var meminfo = _parseProcMeminfo();
+  return _free(meminfo, units);
+}
+
+function total(units) {
+  var meminfo = _parseProcMeminfo();
+  return _total(meminfo, units);
+}
+
+function freePercent() {
+  var meminfo = _parseProcMeminfo();
+  return _freePercent(meminfo);
+}
+
+function usedPercent() {
+  var meminfo = _parseProcMeminfo();
+  return _usedPercent(meminfo);
+}
+
+function allStats(units) {
+  units = units || 'bytes';
+  var meminfo = _parseProcMeminfo();
+  return {
+    free: _free(meminfo, units),
+    total: _total(meminfo, units),
+    freePercent: _freePercent(meminfo),
+    usedPercent: _usedPercent(meminfo),
+  };
+}
+
+//raw `/proc/meminfo` as object
+function raw() {
+  return _parseProcMeminfo();
+}
+
+/* PRIVATE */
+
+function _free(meminfo, units) {
+  units = units || 'bytes';
+
+  var freeMem = meminfo.memFree;
+  var cached = meminfo.cached;
+  var buffers = meminfo.buffers;
+
+  var totalFreeMem = freeMem + cached + buffers;
+  var totalFreeMemBytes = totalFreeMem * 1024;
+  var converted = _bytesTo(totalFreeMemBytes, units);
+
+  return converted;
+}
+
+function _total(meminfo, units) {
+  units = units || 'bytes';
+  var memTotal = meminfo.memTotal;
+  var memTotalBytes = memTotal * 1024;
+  var converted = _bytesTo(memTotalBytes, units);
+
+  return converted;
+}
+
+function _freePercent(meminfo) {
+  var freeMem = meminfo.memFree;
+  var cached = meminfo.cached;
+  var buffers = meminfo.buffers;
+  var memTotal = meminfo.memTotal;
+
+  var totalFreeMem = freeMem + cached + buffers;
+  var percent = ((totalFreeMem / memTotal) * 100);
+
+  return percent;
+}
+
+function _usedPercent(meminfo) {
+  var freeMem = meminfo.memFree;
+  var cached = meminfo.cached;
+  var buffers = meminfo.buffers;
+  var memTotal = meminfo.memTotal;
+
+  var totalFreeMem = freeMem + cached + buffers;
+  var used = memTotal - totalFreeMem;
+  var percent = ((used / memTotal) * 100);
+
+  return percent;
+}
+
+//NOTE: docs at `man proc`
+function _parseProcMeminfo() {
+  var meminfo = fs.readFileSync('/proc/meminfo');
+  var lines = meminfo.toString().split('\n');
+
+  //remove last blank line
+  lines.pop();
+
+  var data = {};
+  lines.forEach(function(line) {
+    var row = line.split(':');
+    data[_toCamelCase(row[0])] = parseInt(row[1].trim().split(' ')[0]);
+  });
+
+  return data;
+}
+
+//quick dirty to handle parens and underscores
+function _toCamelCase(str) {
+  var newString = '';
+  var insideParens = false;
+  newString += str[0].toLowerCase();
+  for (var i = 1; i < str.length; i++) {
+    var char = str[i];
+    switch (char) {
+      case ')':
+      case '_':
+        break;
+      case '(':
+        insideParens = true;
+        break;
+      default:
+        if (insideParens) {
+          insideParens = false;
+          newString += char.toUpperCase();
+        } else {
+          newString += char;
+        }
+        break;
+    }
+  }
+
+  return newString;
+}
+
+function _bytesTo(bytes, units) {
+  var KiB = 1024;
+  var MiB = 1024 * KiB;
+  var GiB = 1024 * MiB;
+
+  switch (units) {
+    case 'bytes':
+      break;
+    case 'KiB':
+      bytes /= KiB;
+      break;
+    case 'MiB':
+      bytes /= MiB;
+      break;
+    case 'GiB':
+      bytes /= GiB;
+      break;
+    default:
+      var errMsg =
+        '[mem-stats] Error: Unknown units "' + units + '", use one of: ' +
+        '"bytes" (default), "KiB", "MiB" or "GiB"';
+      console.log(errMsg);
+  }
+
+  //NOTE: the variable named `bytes` may not actually contain a number
+  //representing the number of bytes. its done this way to only have to use one
+  //variable.
+  return bytes;
+}
